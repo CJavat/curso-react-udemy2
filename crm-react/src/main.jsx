0c6f8e448439d6a785ae61f5fd1bb744b22f7e96
JavatoX <cdpm98@hotmail.com>
2022-12-08 12:53:27 -0600

    SECCION 9 - TERMINADO

--- crm-react/src/main.jsx
+++ crm-react/src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./components/Layout";
-import { NuevoCliente } from "./pages/NuevoCliente";
-import { Index } from "./pages/Index";
+import {
+  NuevoCliente,
+  action as nuevoClienteAction,
+} from "./pages/NuevoCliente";
+import { Index, loader as clientesLoader } from "./pages/Index";
+import ErrorPage from "./components/ErrorPage";
+import {
+  EditarCliente,
+  loader as editarClienteLoader,
+  action as editarClienteAction,
+} from "./pages/EditarCliente";
+import { action as eliminarClienteAction } from "./components/Cliente";
 
 const router = createBrowserRouter([
   {
@@ -14,10 +24,25 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
+        loader: clientesLoader,
+        errorElement: <ErrorPage />,
       },
       {
         path: "/clientes/nuevo",
         element: <NuevoCliente />,
+        action: nuevoClienteAction,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/clientes/:clienteId/editar",
+        element: <EditarCliente />,
+        loader: editarClienteLoader,
+        action: editarClienteAction,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/clientes/:clienteId/eliminar",
+        action: eliminarClienteAction,
       },
     ],
   },
--- crm-react/src/pages/EditarCliente.jsx
+++ crm-react/src/pages/EditarCliente.jsx
@@ -1,9 +1,28 @@
-import { useNavigate, Form, useActionData, redirect } from "react-router-dom";
+import {
+  Form,
+  useNavigate,
+  useLoaderData,
+  useActionData,
+  redirect,
+} from "react-router-dom";
+import { obtenerCliente, actualizarCliente } from "../data/clientes";
 import Formulario from "../components/Formulario";
 import { Error } from "../components/Error";
-import { agregarCliente } from "../data/clientes";
 
-export async function action({ request }) {
+export async function loader({ params }) {
+  const cliente = await obtenerCliente(params.clienteId);
+
+  if (Object.values(cliente).length === 0) {
+    throw new Response("", {
+      status: 404,
+      statusText: "No hay resultados",
+    });
+  }
+
+  return cliente;
+}
+
+export async function action({ request, params }) {
   const formData = await request.formData();
   const datos = Object.fromEntries(formData);
 
@@ -27,20 +46,22 @@ export async function action({ request }) {
     return errores;
   }
 
-  await agregarCliente(datos);
+  // Actualizar el cliente.
+  await actualizarCliente(params.clienteId, datos);
 
   return redirect("/");
 }
 
-export const NuevoCliente = () => {
-  const errores = useActionData();
+export const EditarCliente = () => {
   const navigate = useNavigate();
+  const cliente = useLoaderData();
+  const errores = useActionData();
 
   return (
     <>
-      <h1 className="font-black text-4xl text-blue-900 ">Nuevo Cliente</h1>
+      <h1 className="font-black text-4xl text-blue-900 ">Editar Cliente</h1>
       <p className="mt-3 ">
-        Llena todos los campos para registrar un nuevo cliente
+        A continuación podrás modificar los datos de un cliente
       </p>
       <div className="flex justify-end mb-2">
         <button
@@ -55,12 +76,12 @@ export const NuevoCliente = () => {
         {errores?.length &&
           errores.map((error, i) => <Error key={i}>{error}</Error>)}
         <Form method="post" noValidate>
-          <Formulario />
+          <Formulario cliente={cliente} />
 
           <input
             type="submit"
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
-            value="Registrar Cliente"
+            value="Guardar Cambios"
           />
         </Form>
       </div>

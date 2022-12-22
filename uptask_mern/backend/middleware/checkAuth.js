c37ad3785a5a71aabde5ab5515de06e677214e93
JavatoX <cdpm98@hotmail.com>
2022-12-22 12:09:26 -0600

    Seccion 35 - TERMINADO

--- uptask_mern/backend/middleware/checkAuth.js
+++ uptask_mern/backend/middleware/checkAuth.js
@@ -15,7 +15,6 @@ const checkAuth = async (req, res, next) => {
       req.usuario = await Usuario.findById(decoded.id).select(
         "-password -confirmado -token -createdAt -updatedAt -__v"
       );
-      console.log(req.usuario);
       return next();
     } catch (error) {
       return res.status(404).json({ status: "error", msg: "Hubo un error" });
@@ -24,7 +23,7 @@ const checkAuth = async (req, res, next) => {
 
   if (!token) {
     const error = new Error("Token no válido.");
-    res.status(401).json({ msg: error.message });
+    return res.status(401).json({ msg: error.message });
   }
 
   next();

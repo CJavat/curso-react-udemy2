83ae07434ebbae9fe2be8805578348ab8882b61e
JavatoX <cdpm98@hotmail.com>
2022-12-28 13:29:53 -0600

    Seccion 44 - TERMINADO

--- uptask_mern/frontend/src/context/PyectosProvider.jsx
+++ uptask_mern/frontend/src/context/PyectosProvider.jsx
@@ -149,6 +149,43 @@ const ProyectosProvider = ({ children }) => {
     setCargando(false);
   };
 
+  const eliminarProyecto = async (id) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await clienteAxios.delete(`/proyectos/${id}`, config);
+
+      // Sincronizar el State.
+      const proyectosActualizados = proyectos.filter(
+        (proyectoState) => proyectoState._id !== id
+      );
+      setProyectos(proyectosActualizados);
+
+      setAlerta({
+        msg: data.msg,
+        error: false,
+      });
+
+      setTimeout(() => {
+        setAlerta({});
+        navigate("/proyectos");
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <ProyectosContext.Provider
       value={{
@@ -159,6 +196,7 @@ const ProyectosProvider = ({ children }) => {
         mostrarAlerta,
         submitProyecto,
         obtenerProyecto,
+        eliminarProyecto,
       }}
     >
       {children}

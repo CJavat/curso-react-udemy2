c9bf7813de91f0bde09fc39e9ad0c3ed9b95ab24
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2023-02-10 07:06:12 -0600

    Seccion 70 - TERMINADO

---
+++ NodeSend/NodeSend-Servidor/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const conectarDB = require("./config/db");
+
+// Crear el servidor.
+const app = express();
+
+// Conectar a la DB.
+conectarDB();
+
+// Puerto de la app.
+const PORT = process.env.PORT || 4000;
+
+// Habilitar leer los valores de un body.
+app.use(express.json());
+
+// Rutas de la app.
+app.use("/api/usuarios", require("./routes/usuarios"));
+
+// Arrancar la app.
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`El servidor esta funcionando n el puerto ${PORT}`);
+});

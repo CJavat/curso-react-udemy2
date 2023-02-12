df0417ef4c23f6fca51cd597e306bdc8b564be24
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2023-02-12 08:37:08 -0600

    Seccion 78 - EN CURSO

--- NodeSend/nodesend-cliente/src/pages/index.js
+++ NodeSend/nodesend-cliente/src/pages/index.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 import Layout from "@/components/Layout";
 import authContext from "context/auth/authContext";
+import Link from "next/link";
+import DropZone from "@/components/DropZone";
 
 function Index() {
   // Extraer el usuario autenticado del storage.
@@ -13,7 +15,31 @@ function Index() {
 
   return (
     <Layout>
-      <h1>Index</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+          <DropZone />
+
+          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+            <h2 className="text-4xl font-sans font-bold text-gray-800 py-4 ">
+              Compartir archivos de forma sencilla y privada
+            </h2>
+
+            <p className="text-lg leading-loose">
+              <span className="text-red-500 font-bold">ReactNodeSend</span> te
+              permite compartir archivos con cifrados de extremo a extremo y un
+              archivo que es eliminado despues de ser descargado. Asi que puedes
+              mantener lo que compartes en privado y asegurarte de que tus cosas
+              no permanezcan en linea para siempre.
+            </p>
+
+            <Link href="/crear-cuenta">
+              <p className="text-red-500 font-bold text-lg hover:text-red-700">
+                Crea una cuenta para mayores beneficios
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
